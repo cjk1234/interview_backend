874d602db416674d221c8d5351fd4db90f9222c5
cjk1234 <2412715080@qq.com>
2025-10-21 23:07:32 +0800

    对话记录得以显示，并同步给房间内其他用户

--- src/main/java/com/interviewpractice/websocket/ChatController.java
+++ src/main/java/com/interviewpractice/websocket/ChatController.java
@@ -28,20 +28,11 @@ public class ChatController {
     /**
      * 处理聊天消息
      */
-    @MessageMapping("/chat")
-    public void handleChatMessage(@RequestBody ChatMessageRequest chatMessage) {
+    @MessageMapping("/chat/{roomId}")
+    public void handleChatMessage(@RequestBody MessageDTO chatMessage) {
         System.out.println("收到聊天消息: " + chatMessage.getContent());
 
-        ChatMessageResponse response = new ChatMessageResponse();
-        response.setId(System.currentTimeMillis());
-        response.setRoomId(chatMessage.getRoomId());
-        response.setUserId(chatMessage.getUserId());
-        response.setUsername("用户" + chatMessage.getUserId());
-        response.setContent(chatMessage.getContent());
-        response.setMessageType("TEXT");
-        response.setCreatedAt(LocalDateTime.now());
-
         // 广播消息给房间内所有用户
-        messagingTemplate.convertAndSend("/topic/message/" + chatMessage.getRoomId(), response);
+        messagingTemplate.convertAndSend("/topic/message/" + chatMessage.getRoomId(), chatMessage);
     }
 }
